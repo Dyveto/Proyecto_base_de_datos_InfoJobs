91c1fd22faa5cc6a32b9dd2f2b6b0fde82451cfa
Dylan <devegatorresdylan@gmail.com>
2024-11-11 19:28:18 -0500

    Creacion de las consultas que llaman las vistas creadas

---
+++ Vistas_tablas_InfoJobs/Llamado_de_vistas_tablas_InfoJobs.sql
@@ -0,0 +1,59 @@
+-- Visualizar datos de Usuarios
+SELECT * FROM vistaUsuario;
+
+-- Visualizar datos de Candidatos
+SELECT * FROM vistaCandidatos;
+
+-- Visualizar datos de Empleadores
+SELECT * FROM vistaEmpleadores;
+
+-- Visualizar datos de Ofertas de Empleo
+SELECT * FROM vistaOfertas;
+
+-- Visualizar datos de Contratos
+SELECT * FROM vistaContratos;
+
+-- Visualizar datos de Actividades
+SELECT * FROM vistaActividades;
+
+-- Visualizar datos de Bloqueos de Empleadores
+SELECT * FROM vistaBloqueos;
+
+-- Visualizar datos de Contactos RRHH
+SELECT * FROM vistaContactosRRHH;
+
+-- Visualizar datos de Reportes
+SELECT * FROM vistaReportes;
+
+-- Visualizar datos de Idiomas de Candidatos
+SELECT * FROM vistaIdiomasCandidatos;
+
+-- Visualizar datos de Referencias de Candidatos
+SELECT * FROM vistaReferenciasCandidatos;
+
+-- Visualizar datos de Certificaciones de Candidatos
+SELECT * FROM vistaCertificacionesCandidatos;
+
+-- Visualizar datos de Habilidades de Candidatos
+SELECT * FROM vistaHabilidadesCandidatos;
+
+-- Visualizar datos de Postulaciones de Candidatos
+SELECT * FROM vistaPostulaciones;
+
+-- Visualizar datos de Historial Laboral de Candidatos
+SELECT * FROM vistaHistorialLaboral;
+
+-- Visualizar datos de Evaluaciones de Candidatos
+SELECT * FROM vistaEvaluacionesCandidatos;
+
+-- Visualizar datos de Beneficios Laborales de Empleadores
+SELECT * FROM vistaBeneficiosLaborales;
+
+-- Visualizar datos de Actividades de Empleadores
+SELECT * FROM vistaActividadesEmpleadores;
+
+-- Visualizar datos de Feedback de Usuarios
+SELECT * FROM vistaFeedbackUsuarios;
+
+-- Visualizar datos de Preferencias Laborales de Candidatos
+SELECT * FROM vistaPreferenciasLaborales;
